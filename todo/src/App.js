073ded198be6bef671dca63620d7c531426768ec
HanSu-yeon <js7122420@gmail.com>
2024-05-10 10:08:51 +0900

    feat: 필터링

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header/Header";
 import TodoList from "./components/TodoList/TodoList";
 
+const filters = ["all", "active", "completed"];
 function App() {
+  const [filter, setFilter] = useState(filters[0]);
   return (
     <div>
-      <TodoList />
+      {/* 참고: onFilterChange={(filter) => setFilter(filter)} => 
+        인자값(filter)과 호출하는 값이 같으니까 onFilterChange={setFilter} 이렇게 함수의 참조값만 전달해줘도된다*/}
+      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
+      <TodoList filter={filter} />
     </div>
   );
 }
--- todo/src/components/TodoList/TodoList.jsx
+++ todo/src/components/TodoList/TodoList.jsx
@@ -2,12 +2,11 @@ import React, { useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
 import Todo from "../Todo/Todo";
 
-export default function TodoList() {
+export default function TodoList({ filter }) {
   const [todos, setTodos] = useState([
     { idx: "0", text: "item1", status: "active" },
     { idx: "1", text: "item2", status: "active" },
   ]);
-
   const handleAdd = (todo) => {
     //새로운 투두를 todos에 업데이트
     setTodos([...todos, todo]);
@@ -20,11 +19,12 @@ export default function TodoList() {
   const handleDelete = (deleted) => {
     setTodos(todos.filter((t) => t.idx !== deleted.idx));
   };
-
+  //선택한 필터에 해당하는 todo만 들어있음
+  const filtered = getFilteredItems(todos, filter);
   return (
     <section>
       <ul>
-        {todos.map((item) => {
+        {filtered.map((item) => {
           return (
             <Todo
               key={item.idx}
@@ -39,4 +39,11 @@ export default function TodoList() {
       <AddTodo onAdd={handleAdd} />
     </section>
   );
+}
+
+function getFilteredItems(todos, filter) {
+  if (filter === "all") {
+    return todos;
+  }
+  return todos.filter((todo) => todo.status === filter);
 }
