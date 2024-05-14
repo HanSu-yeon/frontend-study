b45cebbdc648286186c14abd1a759b2abd214cb9
HanSu-yeon <js7122420@gmail.com>
2024-05-14 09:12:48 +0900

    update: 로컬스토리지에 저장하는 방식으로 구현

--- todo/src/components/TodoList/TodoList.jsx
+++ todo/src/components/TodoList/TodoList.jsx
@@ -1,26 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
 import Todo from "../Todo/Todo";
 import styles from "./TodoList.module.css";
+
 export default function TodoList({ filter }) {
-  const [todos, setTodos] = useState([
-    { idx: "0", text: "item1", status: "active" },
-    { idx: "1", text: "item2", status: "active" },
-  ]);
+  const [todos, setTodos] = useState([]);
+
   const handleAdd = (todo) => {
     //새로운 투두를 todos에 업데이트
+    let todosString = localStorage.getItem("todos");
+    let todosArr = JSON.parse(todosString);
+    todosArr.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todosArr));
     setTodos([...todos, todo]);
+    // console.log(todosArr);
   };
+
   //check여부
   const handleUpdate = (updated) => {
-    setTodos(todos.map((t) => (t.idx === updated.idx ? updated : t)));
+    const updateTodo = todos.map((t) => (t.idx === updated.idx ? updated : t));
+    localStorage.setItem("todos", JSON.stringify(updateTodo));
+
+    setTodos(updateTodo);
   };
 
   const handleDelete = (deleted) => {
-    setTodos(todos.filter((t) => t.idx !== deleted.idx));
+    let todosString = localStorage.getItem("todos");
+    let todosArr = JSON.parse(todosString);
+    // setTodos(todos.filter((t) => t.idx !== deleted.idx));
+    const deleteTodo = todosArr.filter((t) => t.idx !== deleted.idx);
+    console.log(deleteTodo);
+    localStorage.setItem("todos", JSON.stringify(deleteTodo));
+    setTodos(deleteTodo);
   };
   //선택한 필터에 해당하는 todo만 들어있음
   const filtered = getFilteredItems(todos, filter);
+
+  useEffect(() => {
+    const todoString = localStorage.getItem("todos");
+
+    if (todoString == null) {
+      //key :value 저장
+      const initTodo = [];
+      localStorage.setItem("todos", JSON.stringify(initTodo));
+    } else {
+      const todosArr = JSON.parse(todoString);
+      setTodos([...todosArr]);
+    }
+  }, []);
+
   return (
     <section className={styles.container}>
       <ul className={styles.list}>
