30e270e4610d3a890c1c6c18be6859073c40a303
HanSu-yeon <hsy@rmsoft.kr>
2024-05-21 14:06:24 +0900

    update: mock 데이터 및 화면 로직 추가

--- src/component/TodoList/TodoList.jsx
+++ src/component/TodoList/TodoList.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TodoList() {
-  return <div></div>;
+  const [todos, setTodos] = useState([
+    { id: 1, text: "아이템1", status: "active" },
+    { id: 2, text: "아이템2", status: "active" },
+  ]);
+  const addTodo = (todo) => {
+    setTodos([...todos, todo]);
+  };
+
+  return (
+    <div>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
