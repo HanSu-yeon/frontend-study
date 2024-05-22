a75c55deff176d25b5f5bdff547fc3c8157641d9
HanSu-yeon <hsy@rmsoft.kr>
2024-05-22 11:26:24 +0900

    폴더 꼬임 수정

---
+++ lectures/todo/src/App.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import TodoList from "./components/TodoList/TodoList";
+import { DarkModeProvider } from "./components/context/DarkModeContext";
+
+const filters = ["all", "active", "completed"];
+function App() {
+  const [filter, setFilter] = useState(filters[0]);
+  return (
+    <DarkModeProvider>
+      {/* 참고: onFilterChange={(filter) => setFilter(filter)} => 
+        인자값(filter)과 호출하는 값이 같으니까 onFilterChange={setFilter} 이렇게 함수의 참조값만 전달해줘도된다*/}
+
+      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
+      <TodoList filter={filter} />
+    </DarkModeProvider>
+  );
+}
+
+export default App;
