1334ce10522d5100107786e217cdd7a075092abd
HanSu-yeon <hsy@rmsoft.kr>
2024-05-22 10:56:45 +0900

    update: darkMode context 생성

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./component/Header/Header";
 import TodoList from "./component/TodoList/TodoList";
+import { DarkModeProvider } from "./component/context/DarkModeContext";
 
 const filters = ["all", "active", "completed"];
 function App() {
@@ -12,10 +13,10 @@ function App() {
     setFilter(selected);
   };
   return (
-    <>
+    <DarkModeProvider>
       <Header filter={filter} filters={filters} onFilter={handleFilter} />
       <TodoList filter={filter} />
-    </>
+    </DarkModeProvider>
   );
 }
 
--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,8 +1,20 @@
 import React from "react";
-
+import { IoMoonOutline } from "react-icons/io5";
+import { FaRegSun } from "react-icons/fa6";
+import { useDarkMode } from "../context/DarkModeContext";
 export default function Header({ filters, filter, onFilter }) {
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
+  const handleDarkMode = () => {
+    toggleDarkMode();
+  };
   return (
     <>
+      <button onClick={handleDarkMode}>
+        {!darkMode && <IoMoonOutline />}
+        {darkMode && <FaRegSun />}
+      </button>
+
       <li>
         {filters.map((filter) => {
           return (
